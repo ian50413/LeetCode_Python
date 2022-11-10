2de87b631c540e7ef9cbf24a08a75a79792dc7dd
ian50413 <ian50413@gmail.com>
2022-11-10 16:32:14 +0800

    master

---
+++ two-sum.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 15:49:38 2022
+
+@author: ian50
+
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
