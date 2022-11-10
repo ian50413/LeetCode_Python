621525a7af77112e8026f9ca57b00ee0e29ef877
ian50413 <ian50413@gmail.com>
2022-11-10 16:57:44 +0800

    add twoSum

--- two-sum.py
+++ two-sum.py
@@ -12,10 +12,19 @@ class Solution(object):
         :rtype: List[int]
 
 """
-
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+#%%
+"""
+Implement User-Defined-Functions(UDFs) in class
+
+Learn Classes in Python in 4 min: https://www.youtube.com/watch?v=rJzjDszODTI
+"""
+Ans = Solution()
+Ans.twoSum([1,2,3,4,5,6],11)
+#%%
+
